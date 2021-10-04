e865c88fe569b3ada035de11978f10a22144cee5
Christian Jay Estandarte <89247172+27Christian@users.noreply.github.com>
2021-10-04 08:51:36 +0800

    Add files via upload

--- Estandarte, Christian Jay.c
+++ Estandarte, Christian Jay.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-int main() {
-int num1 = 18;
-printf("I'm Christian Jay Estandarte, age of %d years old",num1);
+#include <stdlib.h>w
+
+int main()
+{
+    int a = 3000, b = 360, c = 1, d = 60, e;
+
+    e = a * b * c / d;
+    printf("A spinning tire makes 3000 revolutions per minutes. Calculate how many degrees it rotates in one second. Then display the degrees. \n");
+    printf("---------------------------------------------------------------------------\n");
+    printf("3000 revolutions per minute x 360° x 1 minute / 60 seconds \n");
+    printf("degrees/second = %d \n", e, "°");
 return 0;
 }
